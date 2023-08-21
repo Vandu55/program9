ae0dcadcb7e95966140c89acbc09afee31326358
Ankit-Kumar4 <113576375+Ankit-Kumar4@users.noreply.github.com>
2023-08-21 22:11:50 +0530

    Add project files.

---
+++ program9/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program9
+{
+    internal class Program
+    {
+        static int[] RemoveDuplicate(int[] array)
+        {
+            HashSet<int> seen = new HashSet<int>();
+            List<int> result = new List<int>();
+            foreach(int num in array)
+            {
+                if(!seen.Contains(num))
+                {
+                    seen.Add(num);
+                    result.Add(num);
+                }
+            }
+            return result.ToArray();
+        }
+        static void Main(string[] args)
+        {
+            int[] inputArray = { 2, 3, 4, 4, 5, 6, 8, 9 };
+            int[] uniqueArray = RemoveDuplicate(inputArray);
+
+            Console.WriteLine("original Array" + string.Join(",", inputArray));
+            Console.WriteLine("Array with Duplicate Removed:" + string.Join(",", uniqueArray));
+            Console.ReadLine();
+        }
+    }
+}
